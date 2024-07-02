5d9fb2dcc081d320f64dd4d00692967e0d3e6905
Edo <139759297+EduardoHernandezGuzman@users.noreply.github.com>
2024-07-02 17:32:23 +0000

    Actualizacion

--- src/app.js
+++ src/app.js
@@ -1,11 +1,6 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+const parrafo = document.getElementById('miParrafo');
+const boton = document.getElementById('miBoton');
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+boton.addEventListener('click', function() {
+    parrafo.textContent = '¡El contenido del párrafo ha cambiado!';
+});
